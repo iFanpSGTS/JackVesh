ca3a1fc71a532ac4603f2771a7c29dac2b9ec8c2
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-09-03 12:15:56 +0700

    Added extension scan

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import sys, core.util as util, os
 def run_intepreter(files):
     intepreterJV = util.JVInterpreter("JV")
     if (len(sys.argv) > 1):
-        for line in open(sys.argv[1], "r"):
-            intepreterJV.interpret_line(line)
+        if (not sys.argv[1].endswith(".jv")):
+            print("That is not the compitable file, try another file with (.jv) ext")
+        else:
+            for line in open(sys.argv[1], "r"):
+                intepreterJV.interpret_line(line)
 
 if __name__ == "__main__":
     try:
