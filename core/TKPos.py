7f178fc89cf211e541ba822e61c7fb1c39044696
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-10-08 13:15:52 +0700

    ErrorHandler 100% work.

---
+++ core/TKPos.py
@@ -0,0 +1,40 @@
+class Position:
+  def __init__(self, idx, ln, col, fn, ftxt):
+    self.idx = idx
+    self.ln = ln
+    self.col = col
+    self.fn = fn
+    self.ftxt = ftxt
+
+  def advance(self, current_char=None):
+    self.idx += 1
+    self.col += 1
+
+    if current_char == '\n':
+      self.ln += 1
+      self.col = 0
+
+    return self
+
+  def copy(self):
+    return Position(self.idx, self.ln, self.col, self.fn, self.ftxt)
+
+class Token:
+  def __init__(self, type_, value=None, pos_start=None, pos_end=None):
+    self.type = type_
+    self.value = value
+
+    if pos_start:
+      self.pos_start = pos_start.copy()
+      self.pos_end = pos_start.copy()
+      self.pos_end.advance()
+
+    if pos_end:
+      self.pos_end = pos_end.copy()
+
+  def matches(self, type_, value):
+    return self.type == type_ and self.value == value
+  
+  def __repr__(self):
+    if self.value: return f'{self.type}:{self.value}'
+    return f'{self.type}'
