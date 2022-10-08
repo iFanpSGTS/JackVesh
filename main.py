b2b655af910c2f1a8146cb743e45f7fcca153ae2
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-10-08 10:34:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,1 @@
-import sys, core.util as util, os
-
-def run_intepreter(files):
-    intepreterJV = util.JVInterpreter("JV")
-    if (len(sys.argv) > 1):
-        if (not sys.argv[1].endswith(".jv")):
-            print("That is not the compitable file, try another file with (.jv) ext")
-        else:
-            for line in open(sys.argv[1], "r"):
-                intepreterJV.interpret_line(line)
-
-if __name__ == "__main__":
-    try:
-        run_intepreter(sys.argv[1])
-    except Exception as err:
-        print(f"Error: {err}")
+pass
