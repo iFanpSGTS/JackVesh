9ccb813317e1b80d3af44129942e6bbc9f3fa400
iFanpS <89406191+iFanpSGTS@users.noreply.github.com>
2022-10-08 10:33:53 +0700

    Add files via upload

---
+++ core/Lexer.py
@@ -0,0 +1,244 @@
+from TKPos import Position, Token;import string;from ErrorHandler import *
+
+DIGITS = '0123456789'
+LETTERS = string.ascii_letters
+LETTERS_DIGITS = LETTERS + DIGITS
+
+KEYWORDS = [
+  'VAR',
+  'AND',
+  'OR',
+  'NOT',
+  'IF',
+  'ELIF',
+  'ELSE',
+  'FOR',
+  'TO',
+  'STEP',
+  'WHILE',
+  'FUN',
+  'THEN',
+  'END',
+  'RETURN',
+  'CONTINUE',
+  'BREAK',
+]
+
+INT				= 'INT'
+FLOAT    	= 'FLOAT'
+STRING			= 'STRING'
+IDENTIFIER	= 'IDENTIFIER'
+KEYWORD		= 'KEYWORD'
+PLUS     	= 'PLUS'
+MINUS    	= 'MINUS'
+MUL      	= 'MUL'
+DIV      	= 'DIV'
+POW				= 'POW'
+EQ					= 'EQ'
+LPAREN   	= 'LPAREN'
+RPAREN   	= 'RPAREN'
+LSQUARE    = 'LSQUARE'
+RSQUARE    = 'RSQUARE'
+EE					= 'EE'
+NE					= 'NE'
+LT					= 'LT'
+GT					= 'GT'
+LTE				= 'LTE'
+GTE				= 'GTE'
+COMMA			= 'COMMA'
+ARROW			= 'ARROW'
+NEWLINE		= 'NEWLINE'
+EOF				= 'EOF'
+
+class Lexer:
+  def __init__(self, fn, text):
+    self.fn = fn
+    self.text = text
+    self.pos = Position(-1, 0, -1, fn, text)
+    self.current_char = None
+    self.advance()
+  
+  def advance(self):
+    self.pos.advance(self.current_char)
+    self.current_char = self.text[self.pos.idx] if self.pos.idx < len(self.text) else None
+
+  def make_tokens(self):
+    tokens = []
+
+    while self.current_char != None:
+      if self.current_char in ' \t':
+        self.advance()
+      elif self.current_char == '#':
+        self.skip_comment()
+      elif self.current_char in ';\n':
+        tokens.append(Token(NEWLINE, pos_start=self.pos))
+        self.advance()
+      elif self.current_char in DIGITS:
+        tokens.append(self.make_number())
+      elif self.current_char in LETTERS:
+        tokens.append(self.make_identifier())
+      elif self.current_char == '"':
+        tokens.append(self.make_string())
+      elif self.current_char == '+':
+        tokens.append(Token(PLUS, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '-':
+        tokens.append(self.make_minus_or_arrow())
+      elif self.current_char == '*':
+        tokens.append(Token(MUL, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '/':
+        tokens.append(Token(DIV, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '^':
+        tokens.append(Token(POW, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '(':
+        tokens.append(Token(LPAREN, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == ')':
+        tokens.append(Token(RPAREN, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '[':
+        tokens.append(Token(LSQUARE, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == ']':
+        tokens.append(Token(RSQUARE, pos_start=self.pos))
+        self.advance()
+      elif self.current_char == '!':
+        token, error = self.make_not_equals()
+        if error: return [], error
+        tokens.append(token)
+      elif self.current_char == '=':
+        tokens.append(self.make_equals())
+      elif self.current_char == '<':
+        tokens.append(self.make_less_than())
+      elif self.current_char == '>':
+        tokens.append(self.make_greater_than())
+      elif self.current_char == ',':
+        tokens.append(Token(COMMA, pos_start=self.pos))
+        self.advance()
+      else:
+        pos_start = self.pos.copy()
+        char = self.current_char
+        self.advance()
+        return [], IllegalCharError(pos_start, self.pos, "'" + char + "'")
+
+    tokens.append(Token(EOF, pos_start=self.pos))
+    return tokens, None
+
+  def make_number(self):
+    num_str = ''
+    dot_count = 0
+    pos_start = self.pos.copy()
+
+    while self.current_char != None and self.current_char in DIGITS + '.':
+      if self.current_char == '.':
+        if dot_count == 1: break
+        dot_count += 1
+      num_str += self.current_char
+      self.advance()
+
+    if dot_count == 0:
+      return Token(INT, int(num_str), pos_start, self.pos)
+    else:
+      return Token(FLOAT, float(num_str), pos_start, self.pos)
+
+  def make_string(self):
+    string = ''
+    pos_start = self.pos.copy()
+    escape_character = False
+    self.advance()
+
+    escape_characters = {
+      'n': '\n',
+      't': '\t'
+    }
+
+    while self.current_char != None and (self.current_char != '"' or escape_character):
+      if escape_character:
+        string += escape_characters.get(self.current_char, self.current_char)
+      else:
+        if self.current_char == '\\':
+          escape_character = True
+        else:
+          string += self.current_char
+      self.advance()
+      escape_character = False
+    
+    self.advance()
+    return Token(STRING, string, pos_start, self.pos)
+
+  def make_identifier(self):
+    id_str = ''
+    pos_start = self.pos.copy()
+
+    while self.current_char != None and self.current_char in LETTERS_DIGITS + '_':
+      id_str += self.current_char
+      self.advance()
+
+    tok_type = KEYWORD if id_str in KEYWORDS else IDENTIFIER
+    return Token(tok_type, id_str, pos_start, self.pos)
+
+  def make_minus_or_arrow(self):
+    tok_type = MINUS
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '>':
+      self.advance()
+      tok_type = ARROW
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def make_not_equals(self):
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      return Token(NE, pos_start=pos_start, pos_end=self.pos), None
+
+    self.advance()
+    return None, ExpectedCharError(pos_start, self.pos, "'=' (after '!')")
+  
+  def make_equals(self):
+    tok_type = EQ
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      tok_type = EE
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def make_less_than(self):
+    tok_type = LT
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      tok_type = LTE
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def make_greater_than(self):
+    tok_type = GT
+    pos_start = self.pos.copy()
+    self.advance()
+
+    if self.current_char == '=':
+      self.advance()
+      tok_type = GTE
+
+    return Token(tok_type, pos_start=pos_start, pos_end=self.pos)
+
+  def skip_comment(self):
+    self.advance()
+
+    while self.current_char != '\n':
+      self.advance()
+
+    self.advance()
